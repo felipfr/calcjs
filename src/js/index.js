1ae7b8c6531f24a602739e4317e1b926c5b262aa
Felipe Rodrigues <99354371+felipfr@users.noreply.github.com>
2023-01-29 19:28:36 -0300

    Source Code

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import '../styles.css';
+import * as calc from './calc.js';
+import { copyToClipboard } from './copy.js';
+import { themeSwitcher } from './theme-switcher.js';
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+    charKeyBtn.addEventListener("click", calc.charKey);
+});
+
+document.getElementById("clear").addEventListener("click", calc.clear);
+document.querySelector("#input").addEventListener("keydown", calc.keyDown);
+document.getElementById("equal").addEventListener("click", calc.calculate);
+document.getElementById("copyToClipboard").addEventListener("click", copyToClipboard);
+document.getElementById("themeSwitcher").addEventListener("click", themeSwitcher);
